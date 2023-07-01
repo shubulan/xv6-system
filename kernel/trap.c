e3ae30fa5198b3a924c34c82a32b631f33ee29f9
gaoyulong <gaoyulong@xiaomi.com>
2023-07-01 23:38:56 +0800

    question 1

--- kernel/trap.c
+++ kernel/trap.c
@@ -67,6 +67,22 @@ usertrap(void)
     syscall();
   } else if((which_dev = devintr()) != 0){
     // ok
+  } else if (r_scause() == 15) { // page fault
+    printf("pagefault processing\n");
+    uint64 va = r_stval();
+    va = PGROUNDDOWN(va);
+    void* pa = kalloc();
+    if (!pa) {
+      printf("pagefault: out of memory pid=%d\n", p->pid);
+      printf("            sepc=%p stval=%p\n", r_sepc(), r_stval());
+      p->killed = 1;
+    } else {
+      if(mappages(p->pagetable, va, PGSIZE, (uint64)pa, PTE_W|PTE_X|PTE_R|PTE_U) != 0){
+        kfree(pa);
+        printf("pagefault: mappages error pid=%d\n", p->pid);
+        p->killed = 1;
+      }
+    }
   } else {
     printf("usertrap(): unexpected scause %p pid=%d\n", r_scause(), p->pid);
     printf("            sepc=%p stval=%p\n", r_sepc(), r_stval());
